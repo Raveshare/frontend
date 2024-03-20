cbedf776ba690918e98e4c6ba2abb1625d750fe9
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-03-20 07:16:27 +0530

    fix: bg remver

--- src/app/editor/sections/bottom-section/bg-remover/BgRemover.jsx
+++ src/app/editor/sections/bottom-section/bg-remover/BgRemover.jsx
@@ -20,94 +20,75 @@ const BgRemover = ({ inSpeedDial }) => {
 
   const handleRemoveBg = async () => {
     const varImageUrl = store.selectedElements[0]?.src;
-    var elementId = store.selectedElements[0]?.id;
-    fnFindPageNo();
+    const elementId = store.selectedElements[0]?.id;
+    const removedBgURL = await fnRemoveBg(varImageUrl, elementId);
 
-    var removedBgURL = await fnRemoveBg(varImageUrl, elementId);
+    if (removedBgURL?.error) {
+      return { error: removedBgURL.error };
+    }
 
-    if (removedBgURL?.error) return;
+    fnAddImageToCanvas(removedBgURL.url);
+    fnUpdateRecipientDataRef(removedBgURL.elementId, elementId);
 
-    // // To Fix CORS error, we append the string with b-cdn url
-    fnAddImageToCanvas(
-      `${replaceImageURL(removedBgURL?.url)}`,
-      varActivePageNo
-    );
-
-    // update the parentRecipientDataRef with the removed bg nft/asset/image data
-    elementId = store.selectedElements[0]?.id;
-    fnUpdateRecipientDataRef(removedBgURL?.elementId, elementId);
-
-    return removedBgURL?.url;
-  };
-
-  // Find the index of the page for which the removed background image needs to be placed
-  const fnFindPageNo = () => {
-    store.pages.map((page) => {
-      page.identifier == store._activePageId;
-      setStActivePageNo(store.pages.indexOf(page));
-      varActivePageNo = store.pages.indexOf(page);
-    });
-  };
-
-  // Function to Add Removed BG image on the Canvas
-  const fnAddImageToCanvas = async (removedBgUrl, varActivePageNo) => {
-    // Add the new removed Bg Image to the Page
-
-    await store.pages[stActivePageNo || varActivePageNo].addElement({
-      type: "image",
-      x: 0.5 * store.width,
-      y: 0.5 * store.height,
-      width: store.selectedElements[0].width,
-      height: store.selectedElements[0].height,
-      src: removedBgUrl,
-      selectable: true,
-      draggable: true,
-      removable: true,
-      resizable: true,
-      showInExport: true,
-    });
+    return { url: removedBgURL.url };
   };
 
   const fnRemoveBg = async (varImageUrl, elementId) => {
     const res = await getRemovedBgS3Link(varImageUrl, elementId);
-    if (res?.data) {
-      return {
-        url: res?.data?.s3link,
-        elementId: res?.data?.id,
-      };
-    } else if (res?.error) {
-      return {
-        error: res?.error,
-      };
+    return res?.data
+      ? { url: res.data.s3link, elementId: res.data.id }
+      : { error: res?.error };
+  };
+
+  const fnAddImageToCanvas = async (removedBgUrl) => {
+    const activePageIndex = store.pages.findIndex(
+      (page) => page.identifier === store._activePageId
+    );
+    if (activePageIndex !== -1) {
+      const selectedElement = store.selectedElements[0];
+      await store.pages[activePageIndex].addElement({
+        type: "image",
+        x: 0.5 * store.width,
+        y: 0.5 * store.height,
+        width: selectedElement.width,
+        height: selectedElement.height,
+        src: removedBgUrl,
+        selectable: true,
+        draggable: true,
+        removable: true,
+        resizable: true,
+        showInExport: true,
+      });
     }
   };
 
-  //  Toast Setup
   const fnCallToast = async () => {
-    // check if image is selected on canvas
     const varImageUrl = store.selectedElements[0]?.src;
-    if (varImageUrl === undefined) {
+    if (!varImageUrl) {
       toast.error("Please select an image to remove background");
       return;
     }
-    const id = toast.loading("Removing Background", { autoClose: 3000 });
-    const res = await handleRemoveBg();
-    if (res) {
-      toast.update(id, {
-        render: "Removed Background", //Check if The toast is working
-        type: "success",
-        isLoading: false,
+
+    const loadingToastId = toast.loading("Removing Background", {
+      autoClose: false,
+    }); // Keep loading toast open indefinitely
+
+    try {
+      const res = await handleRemoveBg();
+      if (res.url) {
+        toast.success("Removed Background", { autoClose: 3000 });
+      } else if (res.error) {
+        toast.error(res.error || "Error removing background", {
+          autoClose: 3000,
+        });
+      }
+    } catch (error) {
+      toast.error("An error occurred while removing background", {
         autoClose: 3000,
-        closeButton: true,
       });
-    } else if (!res) {
-      toast.update(id, {
-        render: "Error in removing background",
-        type: "error",
-        isLoading: false,
-        autoClose: 3000,
-        closeButton: true,
-      });
+    } finally {
+      // Close the loading toast regardless of success or failure
+      toast.dismiss(loadingToastId);
     }
   };
 
