bcd023268afb05e343a198ab73065f794fe1b915
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-06-01 00:25:22 +0530

    remove: degenchain

--- src/data/constant/enabledChain.js
+++ src/data/constant/enabledChain.js
@@ -1,4 +1,4 @@
 import { base } from "viem/chains";
 import { degenChain } from "../network/customChains";
 
-export const LENSPOST_721_ENALBED_CHAINS = [base, degenChain];
+export const LENSPOST_721_ENALBED_CHAINS = [base];
