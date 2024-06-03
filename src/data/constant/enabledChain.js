f6dd6422203b529da1cba882191b7d9e87411a62
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-06-04 01:27:03 +0530

    update: disabled ham cahon

--- src/data/constant/enabledChain.js
+++ src/data/constant/enabledChain.js
@@ -2,4 +2,4 @@ import { base } from "viem/chains";
 import { degen } from "../network/degen";
 import { ham } from "../network/ham";
 
-export const LENSPOST_721_ENALBED_CHAINS = [base, degen, ham];
+export const LENSPOST_721_ENALBED_CHAINS = [base, degen];
