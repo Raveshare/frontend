f9abd978c8a8ab986460b31c02b9a152cabbacc2
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-06-06 18:02:48 +0530

    update: new user profile

--- src/app/editor/sections/top-section/TopbarSection.jsx
+++ src/app/editor/sections/top-section/TopbarSection.jsx
@@ -1,77 +1,79 @@
-import { useAccount, useNetwork, useSwitchNetwork } from 'wagmi'
-import ShareButton from './share/ShareButton'
-import DownloadBtn from './download/DownloadBtn'
-import { ENVIRONMENT } from '../../../../services'
-import ProfileMenu from './user/ProfileMenu'
-import LoginBtn from './auth/LoginBtn'
-import { useAppAuth } from '../../../../hooks/app'
-import { Typography } from '@material-tailwind/react'
-import { EVMWallets, SolanaWallets } from './auth/wallets'
-import Logo from './logo/Logo'
-import PointsBtn from './PointsBtn/PointsBtn'
-import MobileLoginBtn from './auth/MobileLoginBtn'
-import { useContext, useState } from 'react'
-import LoginModal from './auth/LoginModal'
-import { Context } from '../../../../providers/context'
+import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
+import ShareButton from "./share/ShareButton";
+import DownloadBtn from "./download/DownloadBtn";
+import { ENVIRONMENT } from "../../../../services";
+import ProfileMenu from "./user/ProfileMenu";
+import LoginBtn from "./auth/LoginBtn";
+import { useAppAuth } from "../../../../hooks/app";
+import { Typography } from "@material-tailwind/react";
+import { EVMWallets, SolanaWallets } from "./auth/wallets";
+import Logo from "./logo/Logo";
+import PointsBtn from "./PointsBtn/PointsBtn";
+import MobileLoginBtn from "./auth/MobileLoginBtn";
+import { useContext, useState } from "react";
+import LoginModal from "./auth/LoginModal";
+import { Context } from "../../../../providers/context";
 const TopbarSection = () => {
-	const { openedLoginModal } = useContext(Context)
-	const { isAuthenticated } = useAppAuth()
-	const { chain } = useNetwork()
-	const { switchNetwork } = useSwitchNetwork()
+  const { openedLoginModal } = useContext(Context);
+  const { isAuthenticated } = useAppAuth();
+  const { chain } = useNetwork();
+  const { switchNetwork } = useSwitchNetwork();
 
-	const isSupportedChain = () => {
-		if (ENVIRONMENT === 'production') {
-			if (chain?.id === 137) return true
-		} else {
-			if (chain?.id === 80001) return true
-		}
-	}
+  const isSupportedChain = () => {
+    if (ENVIRONMENT === "production") {
+      if (chain?.id === 137) return true;
+    } else {
+      if (chain?.id === 80001) return true;
+    }
+  };
 
-	return (
-		<>
-			<div className="bg-white mb-2 w-full px-3 py-2 sm:overflow-x-auto sm:overflow-y-hidden sm:max-w-[100vw] sticky border">
-				<div className="flex items-center justify-between">
-					<Logo />
+  return (
+    <>
+      <div className="bg-white mb-2 w-full px-3 py-2 sm:overflow-x-auto sm:overflow-y-hidden sm:max-w-[100vw] sticky border">
+        <div className="flex items-center justify-between">
+          <Logo />
 
-					{!isAuthenticated && (
-						<>
-							<div className="hidden md:flex items-center gap-3">
-								<Typography className="font-semibold text-lg">Login with</Typography>
-								<SolanaWallets title="Solana" />
-								<EVMWallets title="EVM" />
-							</div>
-							<MobileLoginBtn />
-						</>
-					)}
+          {!isAuthenticated && (
+            <>
+              <div className="hidden md:flex items-center gap-3">
+                <Typography className="font-semibold text-lg">
+                  Login with
+                </Typography>
+                <SolanaWallets title="Solana" />
+                <EVMWallets title="EVM" />
+              </div>
+              <MobileLoginBtn />
+            </>
+          )}
 
-					{isAuthenticated ? (
-						<div className="flex items-center justify-center space-x-4 md:space-x-6">
-							{/* Discord Links - 19Jul2023 */}
-							<a
-								className="md:w-8 h-8 text-gray-600 md:flex hidden transition-transform transform-gpu hover:scale-125 hover:rotate-180 hover:duration-2000"
-								target="_blank"
-								href="https://discord.gg/yHMXQE2DNb"
-							>
-								<img src="/topbar-icons/iconDiscord.svg" alt="" />
-							</a>
+          {isAuthenticated ? (
+            <div className="flex items-center justify-center space-x-4 md:space-x-6">
+              {/* Discord Links - 19Jul2023 */}
+              <a
+                className="md:w-8 h-8 text-gray-600 md:flex hidden transition-transform transform-gpu hover:scale-125 hover:rotate-180 hover:duration-2000"
+                target="_blank"
+                href="https://discord.gg/yHMXQE2DNb"
+              >
+                <img src="/topbar-icons/iconDiscord.svg" alt="" />
+              </a>
 
-							<div id="fifth-step">
-								<ShareButton />
-							</div>
-							<div>
-								<DownloadBtn />
-							</div>
-							<div className="" id="first-step">
-// 								<ProfileMenu />
-								<PointsBtn/>
-							</div>
-						</div>
-					) : null}
-				</div>
-			</div>
-			{openedLoginModal && <LoginModal />}
-		</>
-	)
-}
+              <div id="fifth-step">
+                <ShareButton />
+              </div>
+              <div>
+                <DownloadBtn />
+              </div>
+              <div className="" id="first-step">
+                {/* <ProfileMenu /> */}
+                <PointsBtn />
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </div>
+      {openedLoginModal && <LoginModal />}
+    </>
+  );
+};
 
-export default TopbarSection
+export default TopbarSection;
